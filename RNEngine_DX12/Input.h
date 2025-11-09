fa07c77aa34230f2debc66ed553b6b808efc8df4
munaharu <rora.m.241217@gmail.com>
2025-11-09 23:03:30 +0900

    入力システムを途中まで

--- RNEngine_DX12/Input.h
+++ RNEngine_DX12/Input.h
@@ -1,33 +1,56 @@
 #pragma once
 
 #include "stdafx.h"
-#include "Singleton.h"
 namespace RNEngine
 {
-	class Input : public SingletonBase<Input>
-	{
-	private:
-		friend class SingletonBase<Input>;
-		Input() = default;
-		~Input() = default;
-
-		enum class State {
-			PRESS,
-			RELEASE,
-			HOLD
+	enum class InputMode {
+		Keyboard,
+		Gamepad
+	};
+	enum class InputState {
+		Pressed,
+		Released,
+		Held
+	};
+	struct InputData {
+		char key;
+		InputMode mode;
+	};
+	struct InputValue {
+		enum class ValueType {
+			Bool, Float2
+		};
+		ValueType m_Type;
+		union {
+			bool m_Bool;
+			struct {
+				float x;
+				float y;
+			} m_Float2;
 		};
 
-		map<wstring, State> m_KeyState; //キーの状態を保存するマップ
-	public:
-		/// <summary>
-		/// キー入力の初期化
-		/// </summary>
-		void Init();
-		/// <summary>
-		/// キー入力の更新
-		/// </summary>
-		void Update();
 	};
+	class InputAction {
+		vector<function<void()>> m_Actions;
+		
+		vector<InputData> m_Inputs;
+		InputState m_State;
 
+		InputValue m_Value;
+	public:
+		template<typename T>
+		T GetValue()const;
+
+		bool IsPressed()const { m_State == InputState::Pressed; }
+		bool IsReleased()const { m_State == InputState::Released; }
+		bool IsHeld()const { m_State == InputState::Held; }
+	};
+	class Input {
+		static unordered_map<string, InputData> m_InputMap;
+	public:
+		//static void RegisterInput(const string& actionName, char key, InputMode mode);
+
+		//static bool IsPressed(const string& actionName);
+	};
 }
 
